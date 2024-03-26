5735b3a51426bbabeea9df2ef744eee442183b64
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2024-03-26 11:00:11 +0900

    Builder (#3)
    * builder  적용을 위한 DTO 추가
    
    * feat : 회원검색용 Dto 추가
    
    * feat : Builder 추가
    
    * test : builder 작동 테스트
    
    * feat : where 절 파라미터 사용하기

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,14 +1,21 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCond;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
 
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
+import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Repository
 public class MemberJpaRepository {
@@ -21,34 +28,100 @@ public class MemberJpaRepository {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    public void save(Member member){
+    public void save(Member member) {
         em.persist(member);
     }
 
-    public Optional<Member> findById(Long id){
+    public Optional<Member> findById(Long id) {
         Member findMember = em.find(Member.class, id);
         return Optional.ofNullable(findMember);
     }
 
-    public List<Member> findAll(){
-        return em.createQuery("select m from Member m",Member.class)
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
     //Querydsl 사용
-    public List<Member> findAll_Querydsl(){
+    public List<Member> findAll_Querydsl() {
         return queryFactory.selectFrom(member).fetch();
     }
 
-    public List<Member> findByUsername(String username){
-        return em.createQuery("select m from Member m where m.username = :username",Member.class)
-                .setParameter("username",username)
+    public List<Member> findByUsername(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
                 .getResultList();
     }
 
-    public List<Member> findByUsername_Querydsl(String username){
+    public List<Member> findByUsername_Querydsl(String username) {
         return queryFactory.selectFrom(member)
                 .where(member.username.eq(username))
                 .fetch();
+    }
+
+    //Builder 사용
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCond cond) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (hasText(cond.getUsername())) {
+            builder.and(member.username.eq(cond.getUsername()));
+        }
+
+        if (hasText(cond.getTeamName())) {
+            builder.and(team.name.eq(cond.getTeamName()));
+        }
+
+        if (cond.getAgeGoe() != null) {
+            builder.and(member.age.goe(cond.getAgeGoe()));
+        }
+
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCond cond) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(cond.getUsername()),
+                        teamNameEq(cond.getTeamName()),
+                        ageGoe(cond.getAgeGoe()),
+                        ageLoe(cond.getAgeLoe()))
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null ? null : member.age.loe(ageLoe);
     }
 }
